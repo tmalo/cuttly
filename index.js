5533ec1cd5fbf7b136e92e9ba9b099b7fd49e85f
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2021-06-24 08:59:08 +0200

    handling localhost

--- index.js
+++ index.js
@@ -3,23 +3,6 @@ const axios = require("axios");
 const querystring = require("querystring");
 const cuttlyURL = "https://cutt.ly/api/api.php";
 
-const stringIsAValidUrl = (s) => {
-  try {
-    new URL(s);
-    return true;
-  } catch (err) {
-    return false;
-  }
-};
-
-function CuttlyError(code) {
-  this.name = "CuttlyError";
-  this.code = code;
-  this.message = cuttlyErrorCode[code];
-  this.stack = new Error().stack;
-}
-CuttlyError.prototype = new Error();
-
 const cuttlyErrorCode = [
   "Not an url",
   "the link has already been shortened",
@@ -29,7 +12,44 @@ const cuttlyErrorCode = [
   "the link has not passed the validation. Includes invalid characters",
   "The link provided is from a blocked domain",
   "OK - the link has been shortened",
+  "Link is local",
 ];
+
+const stringIsAValidUrl = (s) => {
+  try {
+    new URL(s);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+function CuttlyError(code, url) {
+  var instance = new Error(cuttlyErrorCode[code]);
+  instance.code = code;
+  instance.cuttlyUrl = url;
+
+  Object.setPrototypeOf(instance, Object.getPrototypeOf(this));
+  if (Error.captureStackTrace) {
+    Error.captureStackTrace(instance, CuttlyError);
+  }
+  return instance;
+}
+
+CuttlyError.prototype = Object.create(Error.prototype, {
+  constructor: {
+    value: Error,
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  },
+});
+
+if (Object.setPrototypeOf) {
+  Object.setPrototypeOf(CuttlyError, Error);
+} else {
+  CuttlyError.__proto__ = Error;
+}
 
 const buildShareUrl = (cuttlyKey, longurl) => {
   const url = cuttlyURL;
@@ -37,8 +57,8 @@ const buildShareUrl = (cuttlyKey, longurl) => {
     url +
     "?" +
     querystring.stringify({
+      short: encodeURIComponent(longurl),
       key: cuttlyKey,
-      short: encodeURI(longurl),
     })
   );
 };
@@ -56,24 +76,40 @@ exports.shortenUrl = (key, longurl) =>
       return;
     }
 
+    const isLocalhost = /http(s?):\/\/(localhost|127.0.0.1)(.+)/;
+    if (isLocalhost.test(longurl)) {
+      resolve({
+        date: new Date(),
+        shortLink: longurl,
+        fullLink: longurl,
+        title: "",
+        status: 8,
+      });
+      return;
+    }
+
     const instance = axios.create({
       headers: { Accept: "application/json, text/plain, */*" },
     });
-
-    instance.get(buildShareUrl(key, longurl)).then(
-      (response) => {
-        const answer = response.data;
-        if (answer.url.status === 7) {
-          resolve(answer.url);
-        } else if (answer.url.status === 1) {
-          console.log(answer.url);
-          reject(new CuttlyError(answer.url.status));
-        } else {
-          reject(new CuttlyError(answer.url.status));
+    try {
+      const destUrl = buildShareUrl(key, longurl);
+      return instance.get(destUrl).then(
+        (response) => {
+          const answer = response.data;
+          if (answer.url.status === 7) {
+            resolve(answer.url);
+          } else if (answer.url.status === 1) {
+            reject(new CuttlyError(answer.url.status, destUrl));
+          } else {
+            reject(new CuttlyError(answer.url.status, destUrl));
+            // reject(new Error(cuttlyErrorCode[answer.url.status]));
+          }
+        },
+        (error) => {
+          reject(error);
         }
-      },
-      (error) => {
-        reject(error);
-      }
-    );
+      );
+    } catch (ex) {
+      reject(ex);
+    }
   });
